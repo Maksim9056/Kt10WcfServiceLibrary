85576c03ae6cb1d66bd8076237d713b38cd6cf28
Maks <bobreczovm@inbox.ru>
2024-06-03 12:27:08 +0300

    рАБОТАЕТ

--- Client/Program.cs
+++ Client/Program.cs
@@ -39,27 +39,37 @@ namespace Client
 
             //    Console.WriteLine("Response from service: " + response);
             //}
-            EndpointAddress address = new EndpointAddress("http://localhost:8733/Design_Time_Addresses/Kt10WcfServiceLibrary2/Service1/mex");
+            //EndpointAddress address = new EndpointAddress("http://localhost:8733/Design_Time_Addresses/Kt10WcfServiceLibrary2/Service1/mex");
 
-            // Привязка WsHttpBinding с безопасностью на уровне сообщений
-            WSHttpBinding binding = new WSHttpBinding(SecurityMode.Message);
+            //// Привязка WsHttpBinding с безопасностью на уровне сообщений
+            //WSHttpBinding binding = new WSHttpBinding(SecurityMode.Message);
 
-            // Создание канала к службе
-            using (ChannelFactory<ISecureService> factory = new ChannelFactory<ISecureService>(binding, address))
-            {
-                // Установка учетных данных
-                factory.Credentials.UserName.UserName = "username";
-                factory.Credentials.UserName.Password = "password";
+            //// Создание канала к службе
+            //using (ChannelFactory<ISecureService> factory = new ChannelFactory<ISecureService>(binding, address))
+            //{
+            //    // Установка учетных данных
+            //    factory.Credentials.UserName.UserName = "username";
+            //    factory.Credentials.UserName.Password = "password";
 
-                // Создание прокси
-                ISecureService proxy = factory.CreateChannel();
+            //    // Создание прокси
+            //    ISecureService proxy = factory.CreateChannel();
 
-                // Вызов метода службы
-                string response = proxy.SecureMethod("Test message");
+            //    // Вызов метода службы
+            //    string response = proxy.SecureMethod("Test message");
 
-                Console.WriteLine("Response from service: " + response);
-            }
+            //    Console.WriteLine("Response from service: " + response);
+            //}
+            var binding = new WSHttpBinding();
+            binding.Security.Mode = SecurityMode.Message;
+            binding.Security.Message.ClientCredentialType =
+            MessageCredentialType.Windows;
+            var endpoint = new EndpointAddress("http://localhost:8733/Design_Time_Addresses/Kt10WcfServiceLibrary2/Service1/");
+            var channelFactory = new ChannelFactory<ISecureService>(binding, endpoint);
 
+              var proxy = channelFactory.CreateChannel();
+              var s =  proxy.SecureMethod("12");
+            //proxy.SecureMethod();
+            //client.Call Secure Method();
             Console.WriteLine("Press <Enter> to exit.");
             Console.ReadLine();
 
