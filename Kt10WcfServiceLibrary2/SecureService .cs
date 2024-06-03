51f384fb69f76502f0a21025093d8571b7a18bc6
Maks <bobreczovm@inbox.ru>
2024-06-03 12:03:32 +0300

    Добавьте файлы проекта.

---
+++ Kt10WcfServiceLibrary2/SecureService .cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Kt10WcfServiceLibrary2
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде и файле конфигурации.
+    public class Service1 : ISecureService
+    {
+        public string SecureMethod(string input)
+        {
+            return "Response from SecureMethod: " + input;
+        }
+
+    }
+}
